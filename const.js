7fa0ca61e8eff082cf97c5c5114004e0959ebcd1
Constantine <kshmirko@gmail.com>
2022-09-20 13:16:27 +1000

    Ntsn

---
+++ const.js
@@ -0,0 +1,20 @@
+const commands = `
+/start - перезапустить бота;
+/help  - помощь.
+`
+
+const text1 = `
+текст для поверки обработчика и <a href="https://youtube.com">Ссылка</a>
+`
+
+const text2 = `
+текст для поверки обработчика и <a href="https://youtube.com">Ссылка</a>
+`
+const text3 = `
+текст для поверки обработчика и <a href="https://youtube.com">Ссылка</a>
+`
+
+module.exports.commands = commands
+module.exports.text1 = text1
+module.exports.text2 = text2
+module.exports.text3 = text3
